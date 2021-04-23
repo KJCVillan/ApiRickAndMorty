2208e6c3d3ba6ce18e3c470058541a7d6c1dc934
Kevin Johan Conde <johancondevillan@gmail.com>
2021-04-23 03:08:08 -0500

    modificamos el characterlist component para conectar la api al componente html

--- src/app/components/pages/characters/characters-list/characters-list.component.ts
+++ src/app/components/pages/characters/characters-list/characters-list.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '@app/shared/interfaces/character.interface';
+import { CharacterService } from '@app/shared/services/character.service';
+
+import { take } from "rxjs/operators";
+
+
+type RequestInfo = {
+  next: string;
+};
 
 @Component({
   selector: 'app-characters-list',
@@ -6,10 +15,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characters-list.component.scss']
 })
 export class CharactersListComponent implements OnInit {
+  characters: Character[]= [];
+  info: RequestInfo = {
+    next: '',
+  };
+  private pageNum = 1;
+  private query: string | undefined;
+  private hideScrollHeight=200;
+  private showScrollHeight= 500;
 
-  constructor() { }
+  constructor(private characterSvc: CharacterService) { }
 
   ngOnInit(): void {
+    this.getDataFromService();
   }
 
-}
+  private getDataFromService():void
+  {
+    this.characterSvc
+    .SearchCharacters(this.query, this.pageNum)
+    .pipe(take(1))
+    .subscribe((res: any)=> {
+      console.log('Response ->', res);
+
+      const {info, results} = res;
+      this.characters = [...this.characters, ...results];
+      this.info = info;
+    })
+    
+  }
+};
